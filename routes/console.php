4ef36411061ce1904398d724a7fbb8dd1e43d914
PalmarHealer <myoserhd111@gmail.com>
2026-01-17 11:53:02 +0100

    add scheduler configuration and schedule PDF archival task

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/*
+|--------------------------------------------------------------------------
+| Scheduled Tasks
+|--------------------------------------------------------------------------
+*/
+
+// Archive the current day's PDF at 23:59 on weekdays
+Schedule::command('pdf:generate today')
+    ->weekdays()
+    ->at('23:59')
+    ->withoutOverlapping()
+    ->onOneServer()
+    ->runInBackground();
